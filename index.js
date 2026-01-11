a867beae876a75cb687ad381fe893bb2a9ce4271
Krishnam Agarwal <krishnamagarwal007@gmail.com>
2026-01-11 18:08:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+requirq('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req, res)=>{res.send('krishnam.com')})
+app.get('/login',(req, res)=>{
+    res.send('<h1> Login to krishnam Agarwal</h1>')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
